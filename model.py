e6e2be6155c9c72b416659fbd9bcd2b2c88e5dba
Khanh Nguyen <107160291+GiaKhanh16@users.noreply.github.com>
2026-07-07 10:38:03 -0500

    Step 5: should_explore

--- model.py
+++ model.py
@@ -36,8 +36,10 @@ def sample_random_action(action_space):
     return int(action_space.sample())
     pass
 
-# Step 5 - should_explore (not yet solved)
-# TODO: implement
+# Step 5 - should_explore
+def should_explore(epsilon, rng):
+    """Return True with probability epsilon using the provided numpy Generator."""
+    return rng.random() < epsilon
 
 # Step 6 - epsilon_greedy_action (not yet solved)
 # TODO: implement
